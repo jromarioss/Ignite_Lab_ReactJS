10d7738c74e8bc6dd3bb63833eac41a7b1bc1a0b
JromarioSs <jromario2014@gmail.com>
2022-06-24 11:20:00 -0300

    'day-04'

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,10 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+
 export const client  = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o7ixpe10w901xmd31s2isc/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
