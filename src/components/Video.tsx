8ae6454924ef6d046a2c9e6a4151052db4575d3c
JromarioSs <jromario2014@gmail.com>
2022-06-25 22:52:34 -0300

    aula05
    aula finalizada

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,23 +1,8 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
-import { gql, useQuery } from "@apollo/client";
 
 import '@vime/core/themes/default.css';
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug ($slug: String) {
-    lesson(where: {slug: $slug}) {
-      title
-      videoId
-      description
-      teacher {
-        bio
-        avatarURL
-        name
-      }
-    }
-  }
-`
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface GetLessonBySlugResponse {
   lesson: {
@@ -37,13 +22,13 @@ interface VideoProps {
 }
 
 export function Video(props: VideoProps) {
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: props.lessonSlug,
     }
   });
 
-  if (!data) {
+  if (!data || !data.lesson) {
     return (
       <div className="flex-1">
         <p>Carregando...</p>
@@ -72,7 +57,8 @@ export function Video(props: VideoProps) {
               {data.lesson.description}
             </p>
 
-            <div className="flex items-center gap-4 mt-6">
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
                 src={data.lesson.teacher.avatarURL}
@@ -84,6 +70,7 @@ export function Video(props: VideoProps) {
                 <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
               </div>
             </div>
+            )}
           </div>
 
           <div className="flex flex-col gap-4">
